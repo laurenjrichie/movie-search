b203139d5da5b68a7ac27ac0cfd0e9ed2bfa0784
laurenjrichie <laurenjrichie@gmail.com>
2015-02-01 09:42:37 -0800

    saving to database, show favorites functional

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -6,11 +6,19 @@ function init() {
   submit = $("div#submit");
   clickSubmit();
   clickTitle();
+  showFavorites();
 }
 
 function clickSubmit() {
   submit.click(function() {
-    $("#movie_results").append("<h2>Results</h2><div class='ui list'></div>");
+    $('#favorites_section').empty();
+    $("#search_section").append("<div id='movie_results' class='column'><h2>Results</h2><div class='ui list'></div>,</div>\
+      <div id='single_movie' class='column'>\
+        <div class='ui two column grid'>\
+          <div id='image' class='column'></div>\
+          <div id='info' class='column'></div>\
+        </div>\
+      </div>");
     ajaxRequest(setUrl());
   })
 }
@@ -34,7 +42,7 @@ function setUrl() {
   return "http://www.omdbapi.com/?s=" + input;
 }
 
-function ajaxRequest(url) { // make API call
+function ajaxRequest(url) {
   return $.ajax(url,
     {dataType: 'json'}
   ).done(function(data) {
@@ -51,17 +59,87 @@ function setHtmlId(jsonData) {
 }
 
 function clickTitle() {
-  $("#movie_results").on("click", "a", function() { // how to not have it work when you click to the right as well?
+  $("#search_section").on("click", "a", function() { // how to not have it work when you click to the right as well?
     $("#image").empty();
     $("#info").empty();
     var url = "http://www.omdbapi.com/?i=" + $(this).parent().parent().attr('data-imdbId'); // this.data
-    $.ajax(url,
-      {dataType: 'json'}
-    ).done(function(data) {
-      var imageElement = "<img src='" + data["Poster"] + "'>"
-      var infoElement = "<p>info here</p>";
+
+    $.ajax(url, {dataType: 'json'}).done(function(data) {
+      new_title = data["Title"];
+      new_image_url = data["Poster"];
+      new_year = data["Year"];
+      new_plot = data["Plot"];
+
+      var imageElement = "<img src='" + new_image_url + "'>";
+      var infoElement = "<h3>" + new_title + "</h3><p>" + new_year + "</p><p>" + new_plot + "</p>";
+      var favoriteElement = '<span id="favorite"><i class="flag icon"></i>Save to favorites</span>';
       $("#image").append(imageElement);
-      $("#info").append(infoElement);
+      $("#info").append(infoElement, favoriteElement);
     });
   });
+
+  $("#search_section").on('click', 'span#favorite', function() {
+    $.ajax("/movies", {
+      type: 'post',
+      data: {
+        movie: {
+          title: new_title,
+          year: new_year,
+          plot: new_plot,
+          image_url: new_image_url,
+          favorite: true,
+          rating: 0,
+        }
+      }
+    }).done(function(data) {
+      console.log("saved");
+    });
+  });
+
+}
+
+function showFavorites() {
+  $("#showfav").click(function() {
+    $('#favorites_section').empty();
+    $.ajax("/movies", {dataType: 'json'}).done(function(data) {
+      $("#search_section").empty();
+
+      var cardGallery = "<div class='ui cards'></div>";
+      $("#favorites_section").append(cardGallery);
+      for(var i = 0; i < data.length; i++) {
+        var favorite_movie = '\
+          <div class="card" data-id="' + data[i].id + '">\
+            <div class="image">\
+              <img src="' + data[i].image_url + '">\
+            </div>\
+            <div class="content">\
+              <div class="header">' + data[i].title + '</div>\
+              <div class="meta">\
+                <a class="group">' + data[i].year + '</a>\
+              </div>\
+            </div>\
+            <div class="extra content">\
+              <a class="right floated created">' + data[i].rating + '</a>\
+              <i class="delete icon"></i>\
+            </div>\
+          </div>';
+        $(".cards").append(favorite_movie);
+      }
+
+    });
+  });
+
+  $("#favorites_section").on('click', 'i.delete', function(event) {
+    var id = $(this).parent().parent().data('id');
+    var url = '/movies/' + id;
+
+    $.ajax(url, {type: 'delete'}).done(function(data) {
+      var card = $('div.card[data-id="' + data.id + '"]');
+      card.fadeOut(function() {
+        card.remove();
+      });
+    });
+  });
+
+
 }
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,30 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @movies }
+    end
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      render json: @movie
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    render json: @movie
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :plot, :year, :image_url, :favorite, :rating)
+  end
 end
